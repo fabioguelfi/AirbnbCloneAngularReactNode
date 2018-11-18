120fcf12fe5200406edf92cc34e2f680b49f0a25
fabioguelfi <fabioguelfiunix@gmail.com>
2018-11-18 19:34:32 -0200

    Rental Module

--- front-end-angular/src/app/app.module.ts
+++ front-end-angular/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { RentalComponent } from './rental/rental.component';
 import { TempComponent } from './temp/temp.component';
+import { RentalListComponent } from './rental/rental-list/rental-list.component';
+import { RentalListItemComponent } from './rental/rental-list-item/rental-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RentalComponent,
-    TempComponent
+    TempComponent,
+    RentalListComponent,
+    RentalListItemComponent
   ],
   imports: [
     BrowserModule,
