e2ccf3a848f71716d17b4723de672ecc861e2c36
fabioguelfi <fabioguelfiunix@gmail.com>
2018-11-18 19:30:25 -0200

    Introduce routing

--- front-end-angular/src/app/app.module.ts
+++ front-end-angular/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { RentalComponent } from './rental/rental.component';
+import { TempComponent } from './temp/temp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RentalComponent,
+    TempComponent
   ],
   imports: [
     BrowserModule,
