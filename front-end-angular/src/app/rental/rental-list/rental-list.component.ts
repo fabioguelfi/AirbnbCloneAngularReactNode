0eb9bcf8f3c93c46f0d9e2c657a38c1e1899f9a1
fabioguelfi <fabioguelfiunix@gmail.com>
2018-11-18 19:47:34 -0200

    Rental List Component and NgFor

--- front-end-angular/src/app/rental/rental-list/rental-list.component.ts
+++ front-end-angular/src/app/rental/rental-list/rental-list.component.ts
@@ -6,6 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rental-list.component.scss']
 })
 export class RentalListComponent implements OnInit {
+  public rentals: any[] = [{
+    id: 1,
+    title: 'central apartament',
+    city: 'new york',
+    street: 'Times square',
+    category: 'Apartament',
+    image: 'http://via.placeholder.com/350x250',
+    bedrooms: 3,
+    description: 'very nive apartament',
+    daileRate: 34,
+    shared: false,
+    createdAt: '24/12/2017'
+  },
+  {
+    id: 2,
+    title: 'central apartament 2',
+    city: 'new york',
+    street: 'Times square',
+    category: 'Apartament',
+    image: 'http://via.placeholder.com/350x250',
+    bedrooms: 3,
+    description: 'very nive apartament',
+    daileRate: 34,
+    shared: false,
+    createdAt: '24/12/2017'
+  },
+  {
+    id: 3,
+    title: 'central apartament 3',
+    city: 'new york',
+    street: 'Times square',
+    category: 'Apartament',
+    image: 'http://via.placeholder.com/350x250',
+    bedrooms: 3,
+    description: 'very nive apartament',
+    daileRate: 34,
+    shared: false,
+    createdAt: '24/12/2017'
+  }]
 
   constructor() { }
 
--- front-end-angular/src/app/rental/rental.module.ts
+++ front-end-angular/src/app/rental/rental.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalComponent } from './rental.component';
+import { CommonModule } from '@angular/common';
 @NgModule({
     declarations: [
         RentalListComponent,
         RentalListItemComponent,
         RentalComponent
     ],
-    imports: [],
+    imports: [
+        CommonModule
+    ],
     providers: [],
     exports: [
         RentalListComponent,
