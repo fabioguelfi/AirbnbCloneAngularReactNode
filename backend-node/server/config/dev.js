c3e365171b866a5e6d2afffbc63c9ab51515c79e
fabioguelfi <fabioguelfiunix@gmail.com>
2018-11-18 22:08:48 -0200

    Database introduction and creation

---
+++ backend-node/server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    DB_URI: 'mongodb://root:abc123456@ds211724.mlab.com:11724/bwm-ng-dev'
+}
--- backend-node/server/index.js
+++ backend-node/server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/dev');
+
+mongoose.connect(config.DB_URI)
 
 app.get('/rentals', (req, res) => {
     res.json({ 'success': true })
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3007;
 
 app.listen(PORT, () => {
     console.log('i am running')
