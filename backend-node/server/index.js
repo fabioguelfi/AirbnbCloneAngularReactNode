1301a497c1f400b55684dc5e345d9ba6c6b8dbb9
fabioguelfi <fabioguelfiunix@gmail.com>
2018-11-18 22:33:18 -0200

    Separate Routing

--- backend-node/server/index.js
+++ backend-node/server/index.js
@@ -3,15 +3,14 @@ const app = express();
 const mongoose = require('mongoose');
 const config = require('./config/dev');
 const FakeDB = require('./models/fake-db');
+const rentalRoutes = require('./routes/rentals');
 
 mongoose.connect(config.DB_URI).then(() => {
     const fakeDB = new FakeDB();
     fakeDB.seedDB();
 })
 
-app.get('/rentals', (req, res) => {
-    res.json({ 'success': true })
-})
+app.use('/api/v1/rentals', rentalRoutes)
 
 const PORT = process.env.PORT || 3007;
 
